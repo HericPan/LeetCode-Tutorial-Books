10c5e809b1e81da33b01053cf9628e37f2924823
Heric <1214423184@qq.com>
2021-04-02 15:19:05 +0800

    重新整理文件内容

---
+++ LeetCode 初级算法/01 数组/02 买卖股票的最佳时机/C++/cmake-build-debug/CMakeFiles/hericC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hericC.dir/main.cpp.obj"
+  "hericC.exe"
+  "hericC.exe.manifest"
+  "hericC.pdb"
+  "libhericC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hericC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ LeetCode 初级算法/01 数组/02 买卖股票的最佳时机/Python/01 自己首先想到的解题思路.py
@@ -0,0 +1,6 @@
+def maxProfit(prices: list[int]) -> int:
+    pass
+
+print(maxProfit([7,1,5,3,6,4]) == 7)
+print(maxProfit([1,2,3,4,5]) == 4)
+print(maxProfit([7,6,4,3,1]) == 1)
